b995a732a06afe59985761e8ff546d50897f6e86
Sylvester <metagumi@outlook.com>
2018-06-06 19:46:21 -0400

    Commit getWarmthNoQualifier, mixOptimized

--- src/colors/TestColors.kt
+++ src/colors/TestColors.kt
@@ -6,10 +6,11 @@ import colors.Color.*
 fun main(args: Array<String>) {
     println(getMnemonic(Color.BLUE))
     println(getWarmth(Color.ORANGE))
-    println(mix(Color.YELLOW, RED))
-    println(mix(Color.BLUE, RED))
-
-
+    println(getWarmthNoQualifier(ORANGE))
+    println(mix(Color.YELLOW, Color.RED))
+    //println(mix(Color.BLUE, RED))
+    println(mixOptimized(Color.BLUE, Color.YELLOW))
+    println(mixOptimized(Color.RED, Color.BLUE))
 }
 
 fun getMnemonic(color: Color) =
@@ -30,10 +31,33 @@ fun getWarmth(color: Color) =
             Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
         }
 
+fun getWarmthNoQualifier(color: Color) =
+        when (color) {
+            RED, ORANGE, YELLOW -> "warm"
+            GREEN -> "neutral"
+            BLUE, INDIGO, VIOLET -> "cold"
+        }
+
 fun mix( c1: Color, c2: Color ) =
         when( setOf (c1, c2)) {
             setOf(RED, YELLOW) -> ORANGE
             setOf(YELLOW, BLUE) -> GREEN
             setOf(BLUE, VIOLET) -> INDIGO
             else -> throw Exception("Dirty color")
+        }
+
+
+// more efficient than mix function
+fun mixOptimized(c1: Color, c2: Color) =
+        when {
+            (c1 == RED && c2 == YELLOW) ||
+                    (c1 == YELLOW && c2 == RED) -> ORANGE
+
+            (c1 == YELLOW && c2 == BLUE) ||
+                    (c1 == BLUE && c2 == YELLOW) -> GREEN
+
+            (c1 == BLUE && c2 == VIOLET) ||
+                    (c1 == VIOLET && c2 == BLUE) -> INDIGO
+
+            else ->throw Exception ("Dirty color")
         }
